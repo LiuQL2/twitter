a8e7fdf970adeff9f10172adcafa1a7a42619045
Liu Qianlong - eHealth win10 <LiuQL2@sina.com>
2016-12-05 20:36:53 +0800

    fixed bugs in community_top_words_wordCloud.py

--- process/network/statistic_community_size.py
+++ process/network/statistic_community_size.py
@@ -7,7 +7,7 @@ from utility.functions import read_csv_as_dataFrame_by_chunk
 import csv
 
 
-def get_community_nodes(path, file_name,community_size_file,sep, names):
+def get_community_nodes(userId_communityId_file_path_name,community_size_file,path_save_to,sep, names):
     """
     根据(user_id, community_id)格式的文件统计每一个社区里面的人数，保存的文件格式为（community_id, number_of_user).
     :param path:(user_id, community_id)w文件的存储位置，以及（community_id, number_of_user)文件的保存位置。
@@ -17,11 +17,11 @@ def get_community_nodes(path, file_name,community_size_file,sep, names):
     :param names:以pandas数据框的格式读取上述文件，其中列的名称。
     :return:无返回内容。
     """
-    nodes_dataFrame = pd.read_csv(path + file_name,index_col=False, sep = sep,header=None, names = names,dtype = {'user_id':np.str,'community':np.int32})
+    nodes_dataFrame = pd.read_csv(userId_communityId_file_path_name,index_col=False, sep = sep,header=None, names = names,dtype = {'user_id':np.str,'community':np.int32})
     print 'get it'
     print nodes_dataFrame
     community_list = set(list(nodes_dataFrame.community))
-    community_file = open(path + community_size_file, 'wb')
+    community_file = open(path_save_to + community_size_file, 'wb')
     writer = csv.writer(community_file)
     writer.writerow(['community_id', 'number_of_user'])
     for community in community_list:
@@ -30,4 +30,10 @@ def get_community_nodes(path, file_name,community_size_file,sep, names):
     community_file.close()
 
 
-get_community_nodes(path ='D:/LiuQL/eHealth/twitter/visualization/' , file_name = 'userID_communityID_2016-11-25.txt',community_size_file = 'community_size.csv',sep = ' ', names = ['user_id','community'])
+
+userId_communityId_file_path = '/pegasus/harir/yangjinfeng/commitresult/community2/'
+path_save_to = '/pegasus/harir/Qianlong/data/network/community_size/'
+userId_communityId_file_list = get_dirlist(userId_communityId_file_path,key_word_list=['node-com.txt'])
+for file in userId_communityId_file_list:
+    community_size_file_name = file.replace('.icpm.node-com.txt', '_communityId_size.csv')
+    get_community_nodes(userId_communityId_file_path_name=userId_communityId_file_path+file,path_save_to=path_save_to,community_size_file = community_size_file_name,sep = ',', names = ['user_id','community'])
--- process/word_cloud/community_top_words_wordCloud.py
+++ process/word_cloud/community_top_words_wordCloud.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+"""
+用来处理社区的推文内容的关键词，生成词云。
+"""
 import pandas as pd
 import numpy as np
 import json
@@ -8,6 +10,7 @@ from wordcloud import WordCloud
 from utility.functions import get_dirlist
 import chardet
 import sys
+import time
 import unicodedata
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -15,6 +18,12 @@ sys.setdefaultencoding('utf-8')
 
 class commmunityTopWordCloud(object):
     def __init__(self, top_words_path_file, background_color = 'white', font_path = None):
+        """
+        初始化一个实例，用来处理社区的keywords
+        :param top_words_path_file: 文件的路径。
+        :param background_color:背景色，默认白色。
+        :param font_path:字体路径，默认为空。
+        """
         self.font_path = font_path
         self.background_color = background_color
         self.top_words_path_file = top_words_path_file
@@ -24,11 +33,21 @@ class commmunityTopWordCloud(object):
         for line in top_words_file:
             row = json.loads(line)
             community_id = int(row.keys()[0])
-            self.community_id_list.append(community_id)
-            self.community_top_words[community_id] = row.values()[0]
+            if len(row.values()[0]) == 0:
+                pass
+            else:
+                self.community_id_list.append(community_id)
+                self.community_top_words[community_id] = row.values()[0]
         top_words_file.close()
 
-    def plot_word_cloud(self,image_save_to,community_id_list = list()):
+    def plot_word_cloud(self,image_save_to,community_id_list = list(),file_name_key_word = ''):
+        """
+        画出社区hashtags和keywords。当传入的id_list为空时，表示输出所有的社区。
+        :param image_save_to: 照片需要保存的位置
+        :param community_id_list: 需要输出词云的社区。
+        :param file_name_key_word: 保存图片时，图片的名字中含有的关键字
+        :return: 无返回内容。
+        """
         import matplotlib.pyplot as plt
         temp_community_id_list = []
         community_id_list = list(set(community_id_list))
@@ -43,20 +62,35 @@ class commmunityTopWordCloud(object):
             word_cloud = WordCloud(font_path=self.font_path,background_color=self.background_color).generate(self.community_top_words[community_id])
             plt.imshow(word_cloud)
             plt.axis('off')
-            plt.title('community id: ' + str(community_id) + ', top words')
-            plt.savefig(image_save_to + 'community_' + str(community_id) + '_topic.png')
+            # plt.title('community id: ' + str(community_id) + ','+ file_name_key_word)
+            plt.savefig(image_save_to + 'community_' + str(community_id) + '_' +  file_name_key_word +  '.png')
+            print 'community id:', community_id, file_name_key_word,' word cloud has been saved to', image_save_to
 
 if __name__ == '__main__':
-
-    top_words_path_file = ''
+    directory_list = ['2016_03_23','2016_03_24','2016_03_25','2016_03_26','2016_03_27','2016_03_28','2016_03_29','2016_03_30','2016_03_31','total']
+    top_words_path = 'D:/LiuQL/eHealth/twitter/wordCloud/community_top_words/'
     background_color = 'white'
     font_path = 'C:/Windows/fonts/Arial/arial.ttf'
     image_save_to = 'D:/LiuQL/eHealth/twitter/wordCloud/word_cloud_image/community_top_words_image/'
 
-
+    words_type_list = ['hashtags','keywords']
     # top_words_path_file = ''
     # background_color = 'white'
     # font_path = None
     # image_save_to = '/pegasus/harir/Qianlong/data/word_cloud_image/community_top_words_image/'
-    cloud = commmunityTopWordCloud(top_words_path_file=top_words_path_file,background_color=background_color,font_path=font_path)
-    cloud.plot_word_cloud(image_save_to=image_save_to,community_id_list=[])
+    # cloud = commmunityTopWordCloud(top_words_path_file=top_words_path_file,background_color=background_color,font_path=font_path)
+    # cloud.plot_word_cloud(image_save_to=image_save_to,community_id_list=[])
+
+    for directory_name in directory_list:
+        path = top_words_path + directory_name + '/'
+
+        print path
+
+        for words_type in words_type_list:
+            words_file = path + get_dirlist(path,key_word_list=[words_type])[0]
+            print words_file
+            save_to = image_save_to + directory_name + '/' + words_type + '/'
+            cloud = commmunityTopWordCloud(top_words_path_file=words_file,background_color=background_color,font_path=font_path)
+            print 'number of communities:', len(cloud.community_id_list)
+            time.sleep(5)
+            cloud.plot_word_cloud(image_save_to=save_to,file_name_key_word=words_type,community_id_list=[])
